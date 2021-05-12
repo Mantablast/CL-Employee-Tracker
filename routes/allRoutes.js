b3317605096c11d802d03ef5772919152c23dcef
AimeeJay <ajesso@post.com>
2021-05-11 21:37:48 -0400

    connecting the functions

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -5,18 +5,44 @@ const fs = require("fs");
 const path = require("path");
 const express = require('express');
 const app = express();
+const router = require('express').Router();
 //Using express to thoroughly and properly use/"unpack" JSON objects
 app.use(express.urlencoded({ extended: true }));
 
-const router = require('express').Router();
+
 
 function viewAllEmployees() {
+ console.log("you have entered the view all function!");
+}
 
+function viewByDept() {
+    console.log("you have entered the view by dept function!");
+}
+
+function viewByManager() {
+    console.log("you have entered the view by manager function!");
+}
+
+function addEmployee() {
+    console.log("you have entered the add employee function!");
+}
+
+function deleteEmployee() {
+    console.log("you have entered the delete employee function!");
+}
+
+function updateRole() {
+    console.log("you have entered the update role function!");
+}
+
+function updateManager() {
+    console.log("you have entered the update manager function!");
+}
+
+function complete() {
+    console.log("Have a good day. Goodbye!");
 }
 
 
 
-
-
-
-module.exports = allRoutes;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,43 +2,43 @@ const dotenv = require('dotenv');
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const router = require('express').Router();
 // const fs = require("fs");
 const path = require("path");
 const app = express()
 //Using express to thoroughly and properly use/"unpack" JSON objects
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 3001;
-const routes = require('./routes/allRoutes');
+const PORT = process.env.DB_PORT || 3001;
+// const allRoutes = require('./routes/allRoutes');
 const { env } = require('process');
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: process.env.PORT,
+    port: PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     database: 'employees_db'
 })
 connection.connect(function(err){
     if (err) throw err;
-    options();
 })
 
 const CFonts = require('cfonts');
 CFonts.say('Employee | Manager', {
-	font: 'block',              // define the font face
-	align: 'left',              // define text alignment
-	colors: ['magenta', 'cyan'],         // define all colors
-	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-	letterSpacing: 1,           // define letter spacing
-	lineHeight: 1,              // define the line height
-	space: true,                // define if the output text should have empty lines on top and on the bottom
-	maxLength: '0',             // define how many character can be on one line
-	gradient: true,            // define your two gradient colors
-	independentGradient: false, // define if you want to recalculate the gradient for each new line
-	transitionGradient: true,  // define if this is a transition between colors directly
-	env: 'node'                 // define the environment CFonts is being executed in
+	font: 'block',              
+	align: 'left',              
+	colors: ['magenta', 'cyan'],        
+	background: 'transparent',  
+	letterSpacing: 1,         
+	lineHeight: 1,            
+	space: true,                 
+	maxLength: '0',          
+	gradient: true,           
+	independentGradient: false, 
+	transitionGradient: true,  
+	env: 'node'             
 });
 
-const questions1 = () => {
+const questions = () => {
   inquirer.prompt([
     {
       type: "list",
@@ -66,33 +66,32 @@ const questions1 = () => {
     //Depending on answer, send to appropriate function to handle request
     switch (selection.userchoice) {
     case "View all employees":
-        allRoutes.viewAllEmployees();
-        break 
+        viewAllEmployees();
+        break
     case "View all employees By Department":
-        allRoutes.viewByDept();
+        viewByDept();
         break
     case "View all employees By Manager":
-        allRoutes.viewByManager();
+        viewByManager();
         break
     case "Add employee":
-        allRoutes.addEmployee();
+        addEmployee();
         break
     case "Remove employee":
-        allRoutes.deleteEmployee();
+        deleteEmployee();
         break
     case "Update employee role":
-        allRoutes.updateRole();
+        updateRole();
         break
     case "Update employee manager":
-        allRoutes.updateManager();
+        updateManager();
         break
     case "All done for today.":
-        allRoutes.complete();
+        complete();
         break
     }
   })
 };
 
-
-module.exports = router;
+questions();
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
